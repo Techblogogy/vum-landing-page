93540cb9679f91f4ecb0269ed855be06c4f189f7
Fedor Bobylev <fedorbobylev@gmail.com>
2016-08-10 22:55:30 +0300

    Added quiz footer

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function slideOut () {
     $("#vm-test-area").addClass('fadeIn');
     $("#vm-greeting-area").addClass('fadeOut');
 }
+
+var curQst = 1;
+function nextQst() {
+    $("#q"+curQst).addClass('fadeOut');
+    curQst++;
+    $("#q"+curQst).addClass('fadeIn');
+}
