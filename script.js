c1485274e8ac3c3e76bc4b9c3c1d5e78e0c7b829
Fedor Bobylev <fedorbobylev@gmail.com>
2016-08-10 23:10:27 +0300

    Added question switching

--- script.js
+++ script.js
@@ -6,7 +6,17 @@ function slideOut () {
 
 var curQst = 1;
 function nextQst() {
-    $("#q"+curQst).addClass('fadeOut');
-    curQst++;
-    $("#q"+curQst).addClass('fadeIn');
+    $("#q"+curQst).fadeOut(300, function () {
+
+        curQst++;
+        $("#q"+curQst).fadeIn(300);
+    });
+
+    // $("#q"+curQst).animate({ left: "+=50" }, 1000, function () {
+    //
+    // });
+
+    // $("#q"+curQst).effect("slide", {"direction": "left", "mode": "hide"}, 1000);
+    // curQst++;
+    // $("#q"+curQst).effect("slide", {"direction": "right", "mode": "show"}, 1000);
 }
